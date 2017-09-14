d8e47f665e9e17fab8b4b3a6adb8a10f047a6f1a
Alan Badier <abadier@teclib.com>
2017-09-14 10:29:14 +0200

    Adding process class

---
+++ inc/process.class.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ IpPhoneScanner plugin for GLPI
+ Copyright (C) 2017 by the IpPhoneScanner Development Team.
+
+ https://github.com/pluginsGLPI/ipphonescanner
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of IpPhoneScanner.
+
+ IpPhoneScanner is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ IpPhoneScanner is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IpPhoneScanner. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+/**
+* Class to manage the ip range scan and synchronize phones
+* @since 1.0
+*/
+class PluginIpPhoneScannerProcess {
+
+  /**
+   * Cron task to synchro Rudec infos
+   *
+   * @since 1.0
+   * @param CronTask $task the crontask object
+   */
+   public static function cronIPPhoneScanning($task) {
+      global $DB;
+
+      $scanner = new PluginIpPhoneScannerScanner();
+      $scanner->addNetwork('192.168.47.0/24');
+      $scanner->addNetwork('172.20.0.0/16');
+      $scanner->feedThePool();
+   }
+
+   /**
+   * Install process
+   *
+   * @since 1.0
+   * @param Migration $migration the migration object
+   */
+   public static function install(Migration $migration) {
+      Crontask::Register(__CLASS__, 'IPPhoneScanning', DAY_TIMESTAMP,
+                         ['param' => 24,
+                          'mode'  => CronTask::MODE_EXTERNAL]);
+   }
+
+   /**
+    * Plugin uninstall process
+    *
+    * @return boolean
+    */
+   public static function uninstall(Migration $migration) {
+      return true;
+   }
+
+}
